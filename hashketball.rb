e58f7300e1e2267aa55e69b5886a6af2156858ea
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-05-01 08:03:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,74 +117,22 @@ def game_hash
   }
 }
 end
-#access both :home->:players and :away->:players 
-#both players are arrays...find method that iterates through that array and returns :points 
-#must tell program to find it with a name(string) input
-def players
-  home_players = game_hash[:home][:players]
-  away_players = game_hash[:away][:players]
-  total_players = home_players + away_players
-  total_players
-end
 
-def num_points_scored(name)
-  find_player = players.find {|player| player.fetch(:player_name) == name }
-  find_player.fetch(:points)
-end 
 
-num_points_scored("Brendan Haywood")
 
-def shoe_size(name)
-  find_shoe = players.find {|player| player.fetch(:player_name) == name }
-  find_shoe.fetch(:shoe)
-end 
-
-shoe_size("Ben Gordon")
-
-def team_stats 
-  home_team = game_hash.values_at(:home)
-  away_team = game_hash.values_at(:away)
-  total_team_stats = home_team + away_team
-  total_team_stats
-end 
-team_stats 
-
-def team_colors(team_name)
-  find_team = team_stats.find {|team| team.fetch(:team_name) == team_name}
-  find_team.fetch(:colors)
-end 
-team_colors("Brooklyn Nets")
-
-def team_names 
-  team_stats.collect do |team|
-    team[:team_name]
-  end 
-end 
-team_names 
-
-def player_numbers(team_name)
-  find_numbers = team_stats.find {|team| team.fetch(:team_name) == team_name}
-  find_numbers[:players].collect do |player| 
-    player[:number]
-  end 
-end 
-player_numbers("Brooklyn Nets")
-
-# def player_stats(name)
-#   stats = players.find {|stat| stat.fetch(:player_name) == name}
-#   stats
+# def home_team_name 
+#   game_hash.select {|teams| teams[:team_name] } 
 # end 
 
-def player_stats(player_name)
- stats= players.find {|player| player.fetch(:player_name) == player_name}
-  stats.delete_if {|info, string| info == :player_name puts}
+def num_points_scored(player_name)
+  
+  game_hash.map { |team| team[:players].map{|details|details[:points]}}
 end
 
-player_stats("Alan Anderson")
-
-
-def player_with_longest_name
-  most = players.max_by {|player_name| player_name.fetch(:player_name)}
-  most.max {|b,a| b.length <=> a.length}
+def num_points_scored
+  
 end 
-player_with_longest_name
+
+
+
+
